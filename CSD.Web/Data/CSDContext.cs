0d7d28937a3a7277af6c6da0883b225403bb703c
Aigerim <t_sa06@mail.ru>
2023-03-31 19:39:44 +0600

    Added Migration

--- CSD.Web/Data/CSDContext.cs
+++ CSD.Web/Data/CSDContext.cs
@@ -1,6 +1,17 @@
-﻿namespace CSD.Web.Data
+﻿using CSD.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+
+namespace CSD.Web.Data
 {
-    public class CSDContext
+    public class CSDContext:DbContext
     {
+        public CSDContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
     }
 }
